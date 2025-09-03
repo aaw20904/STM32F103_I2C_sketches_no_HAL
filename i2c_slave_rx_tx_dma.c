37de69918af878e15b9e200f007c03a071e1eb51
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2025-09-03 19:02:32 +0300

    Update i2c_slave_rx_tx_dma.c

--- i2c_slave_rx_tx_dma.c
+++ i2c_slave_rx_tx_dma.c
@@ -112,6 +112,7 @@ void main(){
 	  //when busy bug, clean it by reset:
   		I2C1->CR1 |= I2C_CR1_SWRST;
 		HAL_Delay(10);
+	   I2C1->CR1  = 0;
 		MX_I2C1_Init();
 		wrp_i2c_slave_init_DMA(&i2cSlaveHeader);
    }
